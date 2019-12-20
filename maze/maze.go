26b9060328df32488276bf4ef3f560dfcdc56656
tomowarkar <3104kawamoto@gmail.com>
2019-12-21 00:06:11 +0900

    feat: 最短経路探索

--- maze/maze.go
+++ maze/maze.go
@@ -16,6 +16,7 @@ const (
 // Maze :
 type Maze interface {
 	StickDown(seed int64)
+	Solve()
 }
 
 type maze struct {
@@ -104,6 +105,62 @@ func (m *maze) StickDown(seed int64) {
 						break
 					}
 				}
+			}
+		}
+	}
+	m.plt()
+}
+
+func (m *maze) Solve() {
+	start, goal := p{0, 1}, p{m.w - 2, m.h - 2}
+	visited := make([]bool, len(m.data))
+	costs := make([]int, len(m.data))
+	for i := 0; i < len(costs); i++ {
+		costs[i] = len(m.data)
+	}
+	queue := []p{start}
+	costs[start.y*m.w+start.x] = 0
+
+	for len(queue) > 0 {
+		pos := queue[0]
+		queue = queue[1:]
+		visited[pos.y*m.w+pos.x] = true
+
+		for _, v := range []p{up, down, right, left} {
+			x, y := pos.x+v.x, pos.y+v.y
+			currP := p{x, y}
+			if x < 0 || y < 0 || x > m.w-1 || y > m.h-1 {
+				continue
+			}
+			if goal == currP {
+				queue = make([]p, 0)
+				costs[y*m.w+x] = costs[pos.y*m.w+pos.x] + 1
+				break
+			}
+			if m.at(x, y) == path && visited[y*m.w+x] == false {
+				queue = append(queue, currP)
+				costs[y*m.w+x] = costs[pos.y*m.w+pos.x] + 1
+			}
+		}
+	}
+
+	point := goal
+	cost := costs[goal.y*m.w+goal.x]
+	if cost == len(m.data) {
+		return
+	}
+	m.data[goal.y*m.w+goal.x] = route
+	for cost > 1 {
+		for _, v := range []p{up, down, right, left} {
+			x, y := point.x+v.x, point.y+v.y
+			if x < 0 || y < 0 || x > m.w-1 || y > m.h-1 {
+				continue
+			}
+			if costs[y*m.w+x] == cost-1 {
+				cost = costs[y*m.w+x]
+				point = p{x, y}
+				m.data[y*m.w+x] = route
+				break
 			}
 		}
 	}
