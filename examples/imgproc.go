d361eeb45c41cc57f4d745ca5a462ec367fad1b1
tomowarkar <3104kawamoto@gmail.com>
2019-12-25 20:14:50 +0900

    docs: update examples

---
+++ examples/imgproc.go
@@ -0,0 +1,16 @@
+package main
+
+import "github.com/tomowarkar/biome"
+
+func main() {
+	filename := "assets/examples/imgproc.jpg"
+	imgSrc := biome.ReadImage(filename)
+
+	gray := biome.Gray(imgSrc)
+	biome.ToPng("assets/tmp/imgproc_gray", gray)
+	sepia := biome.Sepia(imgSrc)
+	biome.ToPng("assets/tmp/imgproc_sepia", sepia)
+	mosaic := biome.Shade(21, imgSrc)
+	biome.ToPng("assets/tmp/imgproc_mosaic", mosaic)
+
+}
